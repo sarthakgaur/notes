a8d3bbaaf8e16c9440f434fe2fcde1ff5cadaf7e
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2020-05-16 04:07:11 +0530

    Added functions to build storage
    Added get_storage_name and build_storage. Refactoring is required.

--- src/notes.c
+++ src/notes.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,12 +12,18 @@
 #define PATH_TO_CONFIG "/.config/notes/"
 #define CONFIG_FILENAME "config"
 
-// TODO Get the storage location
+#define STORAGE_FN_SIZE 100
+
+// TODO Get the storage location // Done
+// TODO Build the storage location // Done
+// TODO Add comments to functions
 
 void open_config(void);
-void create_config(char *dir_path, char *complete_path);
+void create_config(char *home_str, char *dir_path, char *complete_path);
 void terminate(const char *fmt, ...);
 void print_storage(FILE *fp);
+void build_storage(FILE *fp);
+char *get_storage_name(FILE *fp);
 
 int main(void) {
     open_config();
@@ -29,23 +37,22 @@ void open_config(void) {
     home_str = getenv("HOME");
 
     complete_path = malloc(strlen(home_str) + strlen(PATH_TO_CONFIG) + strlen(CONFIG_FILENAME) + 1);
-    if (complete_path == NULL) {
+    if (complete_path == NULL)
         terminate("%s", "malloc failed in open_config.\n");
-    }
+
     strcpy(complete_path, home_str);
     strcat(complete_path, PATH_TO_CONFIG);
 
     dir_path = strdup(complete_path);
-    if (dir_path == NULL) {
+    if (dir_path == NULL)
         terminate("%s", "strdup failed in open_config.\n");
-    }
 
     strcat(complete_path, CONFIG_FILENAME);
 
     fp = fopen(complete_path, "r");
     if (fp == NULL) {
         // file not found. Create it and add the default location
-        create_config(dir_path, complete_path);
+        create_config(home_str, dir_path, complete_path);
 
         fp = fopen(complete_path, "r");
         if (fp == NULL) {
@@ -54,47 +61,44 @@ void open_config(void) {
         }
 
         printf("2nd attempt success\n");
-
     } else {
         printf("1st attempt success\n");
     }
 
     print_storage(fp);
+    build_storage(fp);
     fclose(fp);
 
     free(dir_path);
     free(complete_path);
 }
 
-void create_config(char *dir_path, char *complete_path) {
+void create_config(char *home_str, char *dir_path, char *complete_path) {
     FILE *fp;
     char *default_dir = "/notes";
+    char *storage_dir;
+
+    storage_dir = malloc(strlen(home_str) + strlen(default_dir) + 1);
+    if (storage_dir == NULL)
+        terminate("%s", "malloc failed in create_config.\n");
+    strcpy(storage_dir, home_str);
+    strcat(storage_dir, default_dir);
 
     struct stat st = {0};
 
-    if (stat(dir_path, &st) == -1) {
-        mkdir(dir_path, 0700);
-    }
+    if (stat(dir_path, &st) == -1)
+        if (mkdir(dir_path, 0700) != 0)
+            terminate("%s", "config folder could not be created\n");
 
     fp = fopen(complete_path, "w");
-    if (fp == NULL) {
-        // Critical error. Terminate the program
+    if (fp == NULL)
         terminate("%s", "Config file could not be created.\n");
-    }
 
-    fprintf(fp, "%s", default_dir);
+    fprintf(fp, "%s", "# If you want to change the storage location, you can do it from here.\n");
+    fprintf(fp, "%s", storage_dir);
 
     fclose(fp);
-}
-
-void terminate(const char *fmt, ...) {
-    va_list ap;
-
-    va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
-    va_end(ap);
-
-    exit(EXIT_FAILURE);
+    free(storage_dir);
 }
 
 void print_storage(FILE *fp) {
@@ -106,5 +110,77 @@ void print_storage(FILE *fp) {
         putchar(ch);
 
     printf("\n");
+}
+
+void build_storage(FILE *fp) {
+    char *storage_folder_name;
+
+    fseek(fp, 0L, SEEK_SET);
+    storage_folder_name = get_storage_name(fp);
+
+    struct stat st = {0};
+
+    if (stat(storage_folder_name, &st) == -1)
+        if (mkdir(storage_folder_name, 0700) != 0)
+            terminate("%s", "storage folder could not be created\n");
+
+    printf("%s", "storage folder created\n");
+}
+
+char *get_storage_name(FILE *fp) {
+    int ch, i = 0;
+    int size = STORAGE_FN_SIZE;
+    char *buffer;
+    bool skip_line = false;
+    bool reading_started = false;
+
+    buffer = malloc(STORAGE_FN_SIZE);
+    if (buffer == NULL)
+        terminate("%s", "malloc failed in get_storage_name.\n");
+
+    while ((ch = fgetc(fp)) != EOF) {
+        if (skip_line && ch == '\n') {
+            skip_line = false;
+            continue;
+        }
+
+        if (skip_line && ch != '\n')
+            continue;
+
+        if (ch == '#' || ch == '\n') {
+            skip_line = true;
+            continue;
+        }
+
+        if (isspace(ch) && !reading_started)
+            continue;
+
+        if (i < size) {
+            reading_started = true;
+            buffer[i++] = ch;
+
+            if (i == size) {
+                size *= 2;
+                buffer = realloc(buffer, size);
+                if (buffer == NULL)
+                    terminate("%s", "realloc failed in get_storage_name.\n");
+            }
+        }
+    }
+
+    buffer[i] = '\0';
+    printf("storage name: %s\n", buffer);
+
+    return buffer;
+}
+
+void terminate(const char *fmt, ...) {
+    va_list ap;
+
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+
+    exit(EXIT_FAILURE);
 }
 
